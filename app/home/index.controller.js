6c261edbf065e529551c0ffcb4999f4f773312c9
EshwarC <EshwarC@hp.com>
2017-09-20 23:36:47 -0700

    first commit

---
+++ app/home/index.controller.js
@@ -0,0 +1,64 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Home.IndexController', Controller);
+
+    function Controller(UserService, FlashService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.search = null;
+        vm.searchData = null;
+        vm.arr = null;
+
+        initController();
+        getSearch();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+
+        function getSearch() {
+            UserService.getSearch()
+                .then(function (data) {
+                    vm.searchData = data;
+                    getList();
+                })
+        }
+
+        vm.executeSearch = function () {
+            alert(vm.selectedName);
+        }
+
+        function executeSearch() {
+            alert(vm.selectedName);
+        }
+
+
+        function getList() {
+            vm.arr = vm.searchData.map(x => x.name);
+        }
+
+        function saveSearch() {
+            var searchTerms = vm.search.obj;
+            var searchArray = Object.keys(searchTerms).map(x => searchTerms[x]);
+            var searchObj = {};
+            debugger;
+            searchObj.name = vm.search.name;
+            searchObj.searchArray = searchArray;
+            UserService.SaveSearch(searchObj)
+                .then(function () {
+                    FlashService.Success('User updated');
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });
+        }
+    }
+
+})();
---
+++ controllers/api/users.controller.js
@@ -0,0 +1,108 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var userService = require('services/user.service');
+
+// routes
+router.post('/authenticate', authenticateUser);
+router.post('/register', registerUser);
+router.get('/current', getCurrentUser);
+router.put('/:_id', updateUser);
+router.delete('/:_id', deleteUser);
+router.post('/saveSearch', saveSearch)
+router.get('/getSearch', getSearch);
+
+module.exports = router;
+
+
+function saveSearch(req, res) {
+    userService.saveSearch(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getSearch(req, res) {
+    userService.getSearch()
+        .then(function (data) {
+            res.send(data);
+        })
+        .catch(function (err) {
+            res.sendStatus(400).send(err);
+        })
+}
+
+function authenticateUser(req, res) {
+    userService.authenticate(req.body.username, req.body.password)
+        .then(function (token) {
+            if (token) {
+                // authentication successful
+                res.send({token: token});
+            } else {
+                // authentication failed
+                res.status(401).send('Username or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function registerUser(req, res) {
+    userService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentUser(req, res) {
+    userService.getById(req.user.sub)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updateUser(req, res) {
+    var userId = req.user.sub;
+    if (req.params._id !== userId) {
+        // can only update own account
+        return res.status(401).send('You can only update your own account');
+    }
+
+    userService.update(userId, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deleteUser(req, res) {
+    var userId = req.user.sub;
+    if (req.params._id !== userId) {
+        // can only delete own account
+        return res.status(401).send('You can only delete your own account');
+    }
+
+    userService.delete(userId)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
